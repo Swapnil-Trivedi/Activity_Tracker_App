acbedeb1d53fa2a8c218f4fc3da0a32ceb542e35
Swapnil-Trivedi <swapniltrivedi1456@gmail.com>
2022-06-19 19:58:49 +0530

    steps and activity endpoints added

--- Backend/API/v1/ActivityAPI/urls.py
+++ Backend/API/v1/ActivityAPI/urls.py
@@ -5,6 +5,6 @@ from .views import ActivityList,ActivityDetail,StepsList,StepsDetail
 urlpatterns = [
     path('activity/',ActivityList.as_view()),
     path('activity/<int:pk>/',ActivityDetail.as_view()),
-    path('steps/<int:pk>/',StepsList.as_view()),
-    path('steps/<int:pk>/<int:pk1>/',StepsDetail.as_view()),
+    path('steps/<int:activity_id>/',StepsList.as_view()),
+    path('steps/<int:activity_id>/<int:pk>/',StepsDetail.as_view()),
 ]
--- Backend/API/v1/ActivityAPI/views.py
+++ Backend/API/v1/ActivityAPI/views.py
@@ -1,8 +1,11 @@
+from http.client import HTTPResponse
+from telnetlib import STATUS
+from urllib import response
 from rest_framework import generics,permissions,viewsets
 from Activity.models import Activity,Steps
 from django.contrib.auth import get_user_model
 from .serializers import ActivitySerializer,StepsSerializer
-
+from rest_framework.exceptions import NotFound 
 
 class ActivityList(generics.ListCreateAPIView):
     queryset=Activity.objects.all()
@@ -16,15 +19,22 @@ class StepsList(generics.ListCreateAPIView):
     serializer_class=StepsSerializer
     # to get based on part of url
     def get_queryset(self):
-        fk=self.kwargs['pk']
-        return Steps.objects.filter(activity_id=fk)
-    
+        activityId=self.kwargs['activity_id']
+        data=Steps.objects.filter(activity_id=activityId).exists()
+        if not data:
+            raise NotFound()
+        else:
+            return Steps.objects.filter(activity_id=activityId)
 
 class StepsDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class=StepsSerializer
 
     def get_queryset(self):
-        activityId=self.kwargs['pk']
-        stepId=self.kwargs['pk1']
-        return Steps.objects.filter(activity_id=activityId,id=stepId)
+        activityId=self.kwargs['activity_id']
+        stepId=self.kwargs['pk']
+        data=Steps.objects.filter(activity_id=activityId).exists()
+        if  data:
+            return Steps.objects.filter(id=stepId,activity_id=activityId)
+        else:
+            return NotFound()
     
