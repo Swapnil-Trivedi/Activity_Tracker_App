43f1e5db81a70fb56248f5dad6116eeb340a449b
Swapnil-Trivedi <swapniltrivedi1456@gmail.com>
2022-06-21 12:50:34 +0530

    added project level permissions

--- Backend/API/v1/ActivityAPI/views.py
+++ Backend/API/v1/ActivityAPI/views.py
@@ -6,16 +6,21 @@ from Activity.models import Activity,Steps
 from django.contrib.auth import get_user_model
 from .serializers import ActivitySerializer,StepsSerializer
 from rest_framework.exceptions import NotFound 
+from .permissions import IsAuthorOrReadOnly
+
 
 class ActivityList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset=Activity.objects.all()
     serializer_class=ActivitySerializer
 
 class ActivityDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset=Activity.objects.all()
     serializer_class=ActivitySerializer
 
 class StepsList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     serializer_class=StepsSerializer
     # to get based on part of url
     def get_queryset(self):
@@ -28,7 +33,7 @@ class StepsList(generics.ListCreateAPIView):
 
 class StepsDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class=StepsSerializer
-
+    permission_classes = (IsAuthorOrReadOnly,)
     def get_queryset(self):
         activityId=self.kwargs['activity_id']
         stepId=self.kwargs['pk']
