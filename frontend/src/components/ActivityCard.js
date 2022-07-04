47870e8a2daba65398a3e07d0ddfd7c357f8ba8c
Swapnil-Trivedi <swapniltrivedi1456@gmail.com>
2022-07-04 12:46:43 +0530

    improved card css

--- frontend/src/components/ActivityCard.js
+++ frontend/src/components/ActivityCard.js
@@ -2,17 +2,27 @@ import React from "react";
 
 function ActivityCard(props) {
   const { ele } = props;
+
   return (
-    <div className="card mx-3 my-3" style={{width: "18rem"}}>
-      <div className="d-flex flex-column card-body">
+    <div className="card mx-3 my-3" style={{ width: "18rem" }}>
+      <div className="card-body">
         <h5 className="card-title">{ele.title}</h5>
-        <p className="card-text">{ele.description} Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum veniam voluptas quaerat veritatis dignissimos et, reiciendis, ipsa esse fugit corrupti rem eum pariatur accusantium iste, adipisci sed iusto repellat cum.</p>
-        <a href="/#" className="btn btn-primary mt-auto">
-          Go somewhere
-        </a>
+        <p className="card-text">
+          {ele.description} Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Rerum veniam voluptas quaerat veritatis dignissimos et,
+          reiciendis, ipsa esse fugit corrupti rem eum pariatur accusantium
+          iste, adipisci sed iusto repellat cum.
+        </p>
+        <hr />
+        <div className="container text-center">
+        <i className="fa-solid fa-pen-to-square mx-3 fa-lg"></i>
+        <i className="fa-solid fa-trash-can fa-lg mx-3"></i>
+        </div>
       </div>
+        
     </div>
   );
 }
 
 export default ActivityCard;
+
